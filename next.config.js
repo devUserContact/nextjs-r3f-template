f04ab02501475248aa767b6e16011502651c8a99
devUserContact <developer.user.contact@protonmail.com>
2022-08-12 21:55:18 -0700

    initial commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+const withTM = require('next-transpile-modules')(['three']) 
+module.exports = withTM() 
