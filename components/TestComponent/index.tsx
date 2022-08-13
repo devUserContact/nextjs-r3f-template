9ac6959293679075984757cc73756288a7e670a9
devUserContact <developer.user.contact@protonmail.com>
2022-08-13 10:22:35 -0700

    created blank template

---
+++ components/TestComponent/index.tsx
@@ -0,0 +1,8 @@
+import React, { useState, useRef, useMemo } from "react";
+import { useFrame, extend } from "@react-three/fiber";
+
+extend({ });
+export const TestComponent = (props: any) => {
+	return;
+};
+export default TestComponent;
--- components/TestComponentCanvas/index.tsx
+++ components/TestComponentCanvas/index.tsx
@@ -1,4 +1,4 @@
-import { TestComponentText } from "../TestComponentText";
+import { TestComponent} from "../TestComponent";
 import { Canvas } from "@react-three/fiber";
 import styles from "../../styles/Home.module.css";
 
@@ -8,7 +8,7 @@ export default function TestComponentCanvas() {
 			<div className={styles.canvasContainer}>
 				<Canvas style={{ width: "100%", height: "100%", background: "white" }}>
 					<ambientLight intensity={0.75} />
-					<TestComponentText />
+					<TestComponent/>
 				</Canvas>
 			</div>
 		</>
