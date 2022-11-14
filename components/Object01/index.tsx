a57b289f4144be7c7f860cf75afdfbf4dbb8bf6d
devUserContact <developer.user.contact@protonmail.com>
2022-11-13 18:46:41 -0800

    added uniforms to template

--- components/Object01/index.tsx
+++ components/Object01/index.tsx
@@ -1,21 +1,33 @@
-import React, { useRef, useCallback } from "react";
+import React, { useRef, useMemo } from "react";
 import { useFrame, shaderMaterial  } from "@react-three/fiber";
 
 import testVert from "../../shaders/testVert.vert";
 import testFrag from "../../shaders/testFrag.frag";
 
 export const Object01 = (props: any) => {
+	
+	const uniforms = useMemo (
+	() => ({
+		u_time: {
+			value: 0.0,
+		},
+	 }),
+	 []
+	);
+
 	const mesh: any = useRef();
 
-	//	useFrame((state, delta) => {
-	//
-	//	});
+	useFrame((state) => {
+		const { clock } = state;
+		mesh.current.material.uniforms.u_time.value = clock.getElapsedTime()
+	});
 	return (
 		<mesh ref={mesh} position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} >
 			<planeGeometry args={[20, 20, 16, 16]} />
 			<shaderMaterial
 				fragmentShader={testFrag}
 				vertexShader={testVert}
+				uniforms={uniforms}
 				wireframe
 			/>
 		</mesh>
--- shaders/testVert.vert
+++ shaders/testVert.vert
@@ -2,11 +2,13 @@
 precision mediump float;
 #endif
 
+uniform float u_time;
+
 void main() {
 	vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-	modelPosition.x += sin(modelPosition.z) * 1.0;
-	modelPosition.y += sin(modelPosition.x) * 2.0;
+	modelPosition.x -= sin(modelPosition.y - u_time) * 2.0;
+	modelPosition.y += sin(modelPosition.x - u_time) * 1.0;
 
 	vec4 viewPosition = viewMatrix * modelPosition;
 	vec4 projectedPosition = projectionMatrix * viewPosition;
