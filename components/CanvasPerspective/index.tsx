d467c43483ead1e9cb031641f4f3bc96eb17b447
devUserContact <developer.user.contact@protonmail.com>
2022-12-28 17:47:31 -0800

    added text component

--- components/CanvasPerspective/index.tsx
+++ components/CanvasPerspective/index.tsx
@@ -4,18 +4,20 @@ import { OrbitControls } from '@react-three/drei'
 
 //import Geometry from "../Geometry";
 //import Particles from "../Particles";
-import Gltf from '../Gltf'
+//import Gltf from '../Gltf'
+import Text from '../Text'
 
 import styles from '../../styles/Main.module.scss'
 
 const CanvasPerspective = () => {
   return (
-    <Canvas camera={{ position: [0, 0, 3] }}>
+    <Canvas camera={{ position: [0, 0, 5] }}>
       <color attach='background' args={['lightgrey']} />
-      <Gltf />
+      <Text />
 {/*
 			<Particles />
 			<Geometry />
+      <Gltf />
 */}
       <OrbitControls />
     </Canvas>
