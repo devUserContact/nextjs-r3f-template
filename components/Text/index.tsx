2987ffbef4e1b80c64f79e3da0c9a79d6f8f7e41
devUserContact <developer.user.contact@protonmail.com>
2022-12-28 17:54:39 -0800

    linting

--- components/Text/index.tsx
+++ components/Text/index.tsx
@@ -1,23 +1,10 @@
-import React, { useMemo, useRef } from 'react'
-import { shaderMaterial, useFrame } from '@react-three/fiber'
-import { UniformsLib, UniformsUtils } from 'three'
+import React, { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
 import { Text3D, Center } from '@react-three/drei'
 
 import myFont from '../../static/UncutSans_Regular.json'
 
-import textVert from '../../shaders/text.vert'
-import textFrag from '../../shaders/text.frag'
-
 export const Text = (props: any) => {
-  var uniforms = UniformsUtils.merge([
-    UniformsLib['lights'],
-    {
-      u_time: { value: 0.0 },
-      lightIntensity: { value: 0.3 },
-      diffuse: { value: [0.2, 0.8, 0.6] },
-    },
-  ])
-
   const mesh: any = useRef()
 
   useFrame((state) => {
@@ -27,26 +14,12 @@ export const Text = (props: any) => {
   return (
     <>
       <pointLight position={[-10, 14, 8]} />
-      <Center top >
-      <Text3D font={myFont} size={1}>
-        devUserContact
-        <meshToonMaterial color={0x00ffff}/>
-      </Text3D>
+      <Center center>
+        <Text3D font={myFont} size={1}>
+          devUserContact
+          <meshToonMaterial color={0x00ffff} />
+        </Text3D>
       </Center>
-
-      {/*
-			<mesh ref={mesh} position={[0, 0, 0]}>
-				<sphereGeometry args={[4, 16, 8]} />
-				<shaderMaterial
-					vertexShader={sphereVert}
-					fragmentShader={sphereFrag}
-					uniforms={uniforms}
-					lights={true}
-					flatShading={true}
-					fog={true}
-				/>
-			</mesh>
-        */}
     </>
   )
 }
