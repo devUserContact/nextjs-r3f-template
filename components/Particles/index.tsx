6f21a52c5f850a087578c5161b30f875195dc038
devUserContact <developer.user.contact@protonmail.com>
2022-12-30 14:33:57 -0800

    linting

--- components/Particles/index.tsx
+++ components/Particles/index.tsx
@@ -1,67 +1,65 @@
-import React, { useRef, useMemo } from "react";
-import { useFrame, shaderMaterial } from "@react-three/fiber";
-import { UniformsUtils, UniformsLib } from "three";
+import React, { useRef, useMemo } from 'react'
+import { useFrame, shaderMaterial } from '@react-three/fiber'
+import { UniformsUtils, UniformsLib } from 'three'
 
-import particleVert from "../../shaders/particle.vert";
-import particleFrag from "../../shaders/particle.frag";
+import particleVert from '../../shaders/particle.vert'
+import particleFrag from '../../shaders/particle.frag'
 
-export const Particles= (props: any) => {
+export const Particles = (props: any) => {
+  const count = 300
 
-	const count =  300;
+  const randomFloat = (min: number, max: number, decimals: number) => {
+    const rand = (Math.random() * (max - min) + min).toFixed(decimals)
+    return rand
+  }
 
-	const randomFloat = (min: number, max: number, decimals: number) => {
-		const rand = (Math.random() * (max - min) + min).toFixed(decimals);
-		return rand;
-	};
+  const particlesPosition = useMemo(() => {
+    const positions = new Float32Array(count * 3)
 
-	const particlesPosition = useMemo(() => {
+    for (let i = 0; i < count; i++) {
+      let x = randomFloat(-5, 5, 2)
+      let y = randomFloat(-5, 5, 2)
+      let z = randomFloat(-5, 5, 2)
 
-		const positions = new Float32Array(count * 3)
+      positions.set([x, y, z], i * 3)
+    }
+    return positions
+  }, [count])
 
-		for (let i = 0; i < count; i++) {
-			let x = randomFloat(-5, 5, 2);
-			let y = randomFloat(-5, 5, 2);
-			let z = randomFloat(-5, 5, 2);
+  const uniforms = useMemo(
+    () => ({
+      u_time: {
+        value: 0.0,
+      },
+    }),
+    []
+  )
 
-			positions.set([x,y,z], i * 3);
-		}
-		return positions;
-	}, [count])
+  const p = useRef()
 
-	const uniforms = useMemo(
-		() => ({
-			u_time: {
-				value: 0.0,
-			},
-		}),
-		[]
-	);
-
-	const p = useRef();
-
-	useFrame((state) => {
-		const { clock } = state;
-		p.current.material.uniforms.u_time.value = clock.getElapsedTime();
-	});
-	return (
-		<>
-			<pointLight position={[-10, 14, 8]} />
-			<points ref={p} >
-				<bufferGeometry attach="geometry">
-				<bufferAttribute
-					attach="attributes-position"
-					count={particlesPosition.length / 3}
-					array= {particlesPosition}
-					itemSize={3}
-				/>
-				</bufferGeometry>
-				<shaderMaterial
-					fragmentShader={particleFrag}
-					vertexShader={particleVert}
-					uniforms={uniforms}
-				/>
-			</points>
-		</>
-	);
-};
-export default Particles;
+  useFrame((state) => {
+    const { clock } = state
+    p.current.material.uniforms.u_time.value = clock.getElapsedTime()
+  })
+  return (
+    <>
+      <pointLight position={[-10, 14, 8]} />
+      <points ref={p}>
+        <bufferGeometry attach='geometry'>
+          <bufferAttribute
+            attach='attributes-position'
+            count={particlesPosition.length / 3}
+            array={particlesPosition}
+            itemSize={3}
+          />
+        </bufferGeometry>
+        <shaderMaterial
+          fragmentShader={particleFrag}
+          vertexShader={particleVert}
+          uniforms={uniforms}
+        />
+      </points>
+    </>
+  )
+}
+export default Particles
